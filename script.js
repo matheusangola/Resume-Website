b34a18e180b1d53e5f5832e41b03e2c5500a27f0
Matheus <93662722+matheusangola@users.noreply.github.com>
2023-04-21 22:58:46 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Function to open list of HTML resources */
+
+function html(){
+    document.getElementById("resources").innerHTML = `<a href="https://www.w3schools.com/html/html_intro.asp" target="_blank">W3schools</a> <br> <br> <a href="https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML/Getting_started">Mozilla</a> <br> <br> <a href="https://devdocs.io/html/">DevDocs</a>`;
+}
+
+/* Function to open list of CSS resources */
+
+function css(){
+    document.getElementById("resources").innerHTML = `<a href="https://www.w3schools.com/css/css_intro.asp" target="_blank">W3schools</a> <br> <br> <a href="https://developer.mozilla.org/en-US/docs/Learn/CSS/First_steps/Getting_started">Mozilla</a> <br> <br> <a href="https://devdocs.io/css/">DevDocs</a>`;
+}
+
+/* Function to open list of JavaScript resources */
+
+function javaScript(){
+    document.getElementById("resources").innerHTML = `<a href="https://www.w3schools.com/js/js_intro.asp" target="_blank">W3schools</a> <br> <br> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">Mozilla</a> <br> <br> <a href="https://devdocs.io/javascript/">DevDocs</a>`;
+}
+
